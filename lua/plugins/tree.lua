a9431ad5ac0f4118b1d3aca3435bc3a428f3a490
Janardhan Jayanth <121275617+JanardhanJayanthxhd@users.noreply.github.com>
2025-11-21 00:02:47 +0530

    update nvim-tree to have guide lines init

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -6,14 +6,27 @@ return {
         "nvim-tree/nvim-web-devicons",
     },
     config = function()
-        require("nvim-tree").setup {}
+        require("nvim-tree").setup {
+            renderer = {
+                indent_markers = {
+                    enable = true,          -- turn on the lines
+                    inline_arrows = false,  -- keep arrows at start of line
+                    icons = {
+                        corner = "└",
+                        edge = "│",
+                        item = "│",
+                        bottom = "─",
+                        none = " ",
+                    },
+                },
+            },
+        }
 
         -- Keymaps
-        vim.keymap.set("n", "<leader>t", function() vim.cmd(":NvimTreeToggle") end)
-        vim.keymap.set("n", "<leader>to", function() vim.cmd(":NvimTreeOpen") end)
-        vim.keymap.set("n", "<leader>tc", function() vim.cmd(":NvimTreeClose") end)
-        vim.keymap.set("n", "<leader>tf", function() vim.cmd(":NvimTreeFocus") end)
-        vim.keymap.set("n", "<leader>tF", function() vim.cmd(":NvimTreeFindFile") end)
-
+        vim.keymap.set("n", "<leader>t", function() vim.cmd("NvimTreeToggle") end)
+        vim.keymap.set("n", "<leader>to", function() vim.cmd("NvimTreeOpen") end)
+        vim.keymap.set("n", "<leader>tc", function() vim.cmd("NvimTreeClose") end)
+        vim.keymap.set("n", "<leader>tf", function() vim.cmd("NvimTreeFocus") end)
+        vim.keymap.set("n", "<leader>tF", function() vim.cmd("NvimTreeFindFile") end)
     end,
 }
