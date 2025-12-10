34d396082cf487c7e04bb06301bad1d48ab83942
janardhanjayanthS <janardhanjayanth@bitcot.com>
2025-12-10 10:49:07 +0530

    update null-ls to fix deprecated warning for real!

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -40,13 +40,13 @@ return {
 			sources = sources,
 			-- you can reuse a shared lspconfig on_attach callback here
 			on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting", { bufnr = bufnr }) then
+				if client.server_capabilities.documentFormattingProvider then
 					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 					vim.api.nvim_create_autocmd("BufWritePre", {
 						group = augroup,
 						buffer = bufnr,
 						callback = function()
-							vim.lsp.buf.format({ async = false })
+							vim.lsp.buf.format({ bufnr = bufnr, async = false })
 						end,
 					})
 				end
